bb64a5d83fa7d6f72bdcb68f71493a800c2e0ffe
ssssyed98 <ssssyed98@gmail.com>
2020-08-01 23:29:54 +0500

    Added redux and set up the proxy

--- client/src/App.js
+++ client/src/App.js
@@ -9,18 +9,28 @@ import {
 import Admin from './layouts/Admin.js';
 import Login from './layouts/Login';
 
+import store from './utils/store';
+import { Provider } from 'react-redux';
+import setAuthToken from './utils/setAuthToken';
+
 import './assets/scss/App.scss';
+
+if (localStorage.getItem('token')) {
+  setAuthToken(localStorage.getItem('token'));
+}
 
 const App = () => {
   return (
     <Fragment>
-      <Router>
-        <Switch>
-          <Route path='/admin' component={Admin} />
-          <Route path='/login' component={Login} />
-          <Redirect from='/' to='/login' />
-        </Switch>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <Switch>
+            <Route path='/admin' component={Admin} />
+            <Route path='/login' component={Login} />
+            <Redirect from='/' to='/login' />
+          </Switch>
+        </Router>
+      </Provider>
     </Fragment>
   );
 };
